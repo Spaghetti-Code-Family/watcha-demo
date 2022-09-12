cc4107e808d42e86adb89da7b9dd677715490baa
Jade <56938630+jadekang0611@users.noreply.github.com>
2022-09-12 15:32:04 -0700

    Set up new pages and router for each page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// Switch is now replaced to Routes per v6
+// no exact is needed
+import { MainPage, MovieDetail, TvDetail, TvPage } from './pages/pages.js';
 
 function App() {
-  return <div>Hello Watcha</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/tv" element={<TvPage />} />
+        <Route path="/movie/:id" element={<MovieDetail />} />
+        <Route path="/tv/:id" element={<TvDetail />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/pages.js
@@ -0,0 +1,6 @@
+import MainPage from './MainPage';
+import MovieDetail from './MovieDetail';
+import TvDetail from './TvDetail';
+import TvPage from './TvPage';
+
+export { MainPage, MovieDetail, TvDetail, TvPage };
