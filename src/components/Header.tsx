a24351d75d1229561c37a48a3d941ffdb601f795
Jade <56938630+jadekang0611@users.noreply.github.com>
2022-09-12 17:59:07 -0700

    Worked on the styling in the header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,38 +1,133 @@
 import React from 'react';
-import { css, jsx } from '@emotion/react';
 import styled from '@emotion/styled';
+import { AiOutlineSearch } from 'react-icons/ai';
 
-const Base = styled.header``;
+const Base = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  backgroundcolor: #fff;
+  text-align: center;
+  box-shadow: rgb(0, 0, 0 / 0%) 0px 1px 0px 0px;
+  width: 100%;
+  height: 62px;
+  z-index: 10;
+`;
 
-const Navigation = styled.nav``;
+const Navigation = styled.nav`
+  margin: 0 auto;
+  max-width: 1200px;
+`;
 
 const MenuListWrapper = styled.div``;
 
-const MenuList = styled.ul``;
+const MenuList = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  overflow: hidden;
+`;
 
-const Menu = styled.li``;
+const Menu = styled.li`
+  display: flex;
+  align-items: center;
+  height: 62px;
+  flex-shrink: 0;
+  &:not(:first-child) {
+    margin-left: 24px;
+  }
+`;
 
-const MenuButton = styled.button``;
+const MenuButton = styled.button<{ active?: boolean }>`
+  font-size: 15px;
+  color: ${({ active }) => (active ? 'rgb(53, 53, 53)' : 'rgb(126, 126. 126)')};
+  cursor: pointer;
+  border: none;
+  background: none;
+`;
 
-const SearchMenu = styled.li``;
+const SearchMenu = styled.li`
+  width: 300px;
+  display: flex;
+  align-items: center;
+  height: 62px;
+  flex-shrink: 1;
+  margin: 0 0 0 auto;
+  position: relative;
+`;
 
-const Link = styled.a``;
+const Link = styled.a`
+  text-decoration: none;
+`;
 
-const TextLogo = styled.h1``;
+const TextLogo = styled.h1`
+  font-size: 24px;
+  font-weight: 700;
+  > span[class='primary'] {
+    color: rgb(255, 47, 110);
+  }
+  > span:not(.primary) {
+    color: #222;
+  }
+`;
 
-const SearchContainer = styled.div``;
+const SearchContainer = styled.div`
+  position: relative;
+  width: 100%;
+`;
 
 const SearchFormWrapper = styled.div``;
 
 const SearchForm = styled.form``;
 
-const SearchLabel = styled.label``;
+const SearchLabel = styled.label`
+  backgroundcolor: rgb(245, 245, 247);
+  display: flex;
+  align-items: center;
+  box-sizing: border-box;
+  width: 100%;
+  height: 38px;
+  border-radius: 2px;
+  padding: 7px 8px;
+`;
 
-const SearchInput = styled.input``;
+const SearchInput = styled.input`
+  font-size: 14px;
+  font-weight: 400;
+  background: transparent;
+  width: 100%;
+  padding: 0 0 0 8px;
+  border: 0;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  caret-color: rgb(53, 53, 53);
+  line-height: 23px;
+`;
 
-const SignIn = styled.button``;
+const SignIn = styled.button`
+  background: transparent;
+  color: rbg(116, 116, 123);
+  font-size: 14px;
+  padding: 0;
+  border: 0;
+  cursor: pointer;
+  margin: 15px 0;
+`;
 
-const SignUp = styled.button``;
+const SignUp = styled.button`
+  border-radius: 6px;
+  font-weight: 500;
+  box-sizing: border-box;
+  min-width: 72px;
+  height: 32px;
+  background: transparent;
+  color: rgb(53, 53, 53);
+  font-size: 14px;
+  border: 1px solid rgba(116, 116, 123, 0.5);
+  cursor: pointer;
+  margin: 15px 0;
+`;
 
 const Header: React.FC = () => {
   const keywordHandler = () => {};
@@ -61,7 +156,9 @@ const Header: React.FC = () => {
               <SearchContainer>
                 <SearchFormWrapper>
                   <SearchForm>
-                    <SearchLabel />
+                    <SearchLabel>
+                        <AiOutlineSearch/>
+                    </SearchLabel>
                     <SearchInput
                       placeholder="Search by contents, name, or collection"
                       onChange={keywordHandler}
@@ -74,7 +171,7 @@ const Header: React.FC = () => {
               <SignIn>Log In</SignIn>
             </Menu>
             <Menu>
-              <SignIn>Sign Up</SignIn>
+              <SignUp>Sign Up</SignUp>
             </Menu>
           </MenuList>
         </MenuListWrapper>
